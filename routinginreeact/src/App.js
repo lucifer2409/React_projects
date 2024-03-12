1f5f6c1aa31c978dac8249a9d1b6cd2bc13dbfe8
Abhishek Trivedi <trivedi.a@somaiya.edu>
2024-03-12 15:08:49 +0530

    Routing in React Revised.
    This is an basic website example where we have used how to do routing in react and practised some concepts of routing in react.

---
+++ routinginreeact/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './Components/Footer/footer';
+import Home from './Components/Home/home';
+import Header from './Components/header/header';
+
+function App() {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
