3ce01c9fd701b98be9f250c58382f699200dc1c4
Abhishek Trivedi <trivedi.a@somaiya.edu>
2024-03-12 13:59:48 +0530

    React Router
    This is a react-router practice file where we have used react-router concept for routing .

---
+++ routinginreeact/src/Components/User/User.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+function User() {
+    const {userid} = useParams()
+  return (
+    <div>
+      <h1 className='bg-gray600 text-white text-3xl'>User:{userid}</h1>
+    </div>
+  )
+}
+
+export default User
